018e35fc300beea0edc1500ee4254ac5e906f062
danilp2 <56089744+danilp2@users.noreply.github.com>
2026-09-21 12:36:08 +0300

    Create BootReceiver.kt

---
+++ java/com/geopedometer/service/BootReceiver.kt
@@ -0,0 +1,22 @@
+package com.geopedometer.service
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.content.ContextCompat
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val serviceIntent = Intent(context, PedometerService::class.java).apply {
+                action = PedometerService.ACTION_START
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                ContextCompat.startForegroundService(context, serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
+        }
+    }
+}
